dbb49cafb563a19103ef4f4528d80ac4883bf0a1
sofia_vasconcellos <79363086+sofiaget@users.noreply.github.com>
2024-03-20 12:25:59 -0300

    Ex.8.py

--- Ex.8.py
+++ Ex.8.py
@@ -1,24 +1,25 @@
-#Método da aceitação-rejeição
 import numpy as np
 import matplotlib.pyplot as plt
 
-n = 100000 #numero de amostras
+n = 100000 # número de amostras
 xfunction = np.array([])
 
 for i in range(n):
-    x1 = np.random.uniform(0, 1)
-    x2 = np.random.uniform(0, 1)
+    x1 = np.random.uniform(-1, 1)
+    x2 = np.random.uniform(0, 2) # cg(x) = 2
 
-    while (x2 >= (6.75) * x1 * pow((1 - x1), 2)): # f(x)/cg(x)
-        x1 = np.random.uniform(0, 1)
-        x2 = np.random.uniform(0, 1)
-    xfunction = np.append(xfunction, x1)#PDF gerada pelo método da aceitação-rejeição
+    while (x2 >= 1.5 * x1**2): # f(x)/cg(x)
+        x1 = np.random.uniform(-1, 1)
+        x2 = np.random.uniform(0, 2)
 
-print(xfunction)
-X = np.arange(0, 1, 0.01)
-fx = 12 * X * pow((1 - X), 2)#f(x) PDF analítica
-#fx1=fx/np.max(fx)
-plt.title('PDF analitica e histograma')
-plt.plot(X, fx)
-plt.hist(xfunction, bins=100, density=True) #Plot dos histogramas da PDF gerada
+    xfunction = np.append(xfunction, x1)
+
+# Plotar a PDF analítica e o histograma das amostras geradas
+X = np.linspace(-1, 1, 1000)
+fx = 1.5 * X**2 # PDF analítica
+
+plt.title('PDF analítica e histograma')
+plt.plot(X, fx, label='PDF analítica')
+plt.hist(xfunction, bins=100, density=True, label='Histograma normalizado') # Plot do histograma das amostras
+plt.legend()
 plt.show()
